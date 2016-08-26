a28279abaef9b2f41a72b0118a5d627edd98bc54
nrjohnstone <locotewz@gmail.com>
2016-08-26 09:22:11 +0200

    implement ambient context service for Serilog

---
+++ src/AmbientContext.LogService.Serilog/AmbientLogService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog;
+
+namespace AmbientContext.LogService.Serilog
+{
+    public class AmbientLogService : AmbientService<ILogger>
+    {
+        protected override ILogger DefaultCreate()
+        {
+            return new SerilogLogAdapter(Log.Logger);
+        }
+    }
+}
---
+++ src/AmbientContext.LogService.Serilog/ILogger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AmbientContext.LogService.Serilog
+{
+    public interface ILogger
+    {
+        void Verbose(string messageTemplate);
+        void Verbose(string messageTemplate, params object[] propertyValues);
+        void Verbose(Exception exception, string messageTemplate, params object[] propertyValues);
+        void Debug(string messageTemplate);
+        void Debug(string messageTemplate, params object[] propertyValues);
+        void Debug(Exception exception, string messageTemplate, params object[] propertyValues);
+        void Information(string messageTemplate);
+        void Information(string messageTemplate, params object[] propertyValues);
+        void Information(Exception exception, string messageTemplate, params object[] propertyValues);
+        void Warning(string messageTemplate);
+        void Warning(string messageTemplate, params object[] propertyValues);
+        void Warning(Exception exception, string messageTemplate, params object[] propertyValues);
+        void Fatal(string messageTemplate);
+        void Fatal(string messageTemplate, params object[] propertyValues);
+        void Fatal(Exception exception, string messageTemplate, params object[] propertyValues);
+        void Error(string messageTemplate);
+        void Error(string messageTemplate, params object[] propertyValues);
+        void Error(Exception exception, string messageTemplate, params object[] propertyValues);
+    }
+}
