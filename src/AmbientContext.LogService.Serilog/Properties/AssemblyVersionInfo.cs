ef96f08b010700b56d3311609a32d54649b58874
nrjohnstone <locotewz@gmail.com>
2016-11-07 12:19:55 +0100

    move version attributes into separate version file

---
+++ src/AmbientContext.LogService.Serilog/Properties/AssemblyVersionInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
+
+[assembly: AssemblyInformationalVersion("0.3.0-unstable.5+Branch.develop.Sha.7b45e8fe0d66af0f8574b8c6f428f76e9045f4e3")]
